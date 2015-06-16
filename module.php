eb39155ad979e9d7ce7ca4c7dcc1a249517f41bd
JustCarmen <carmen@justcarmen.nl>
2015-06-16 08:52:54 +0200

    Add custom namespace and fix uses.

--- module.php
+++ module.php
@@ -14,11 +14,18 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-namespace Fisharebest\Webtrees;
+namespace JustCarmen\WebtreesAddOns\Module;
 
+use Fisharebest\Webtrees\Auth;
 use Fisharebest\Webtrees\Controller\PageController;
+use Fisharebest\Webtrees\Database;
+use Fisharebest\Webtrees\Date;
+use Fisharebest\Webtrees\Filter;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Individual;
 use Fisharebest\Webtrees\Module\AbstractModule;
 use Fisharebest\Webtrees\Module\ModuleConfigInterface;
+use Fisharebest\Webtrees\Tree;
 
 class FancyPrivacyListModule extends AbstractModule implements ModuleConfigInterface {
 
